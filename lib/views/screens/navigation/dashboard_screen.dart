59c06019ab472a8a05e2439d1e6194172794cd4d
Sherazi <sherazahmad720@gmail.com>
2025-04-19 20:36:34 +0500

    dashboard working

--- lib/views/screens/navigation/dashboard_screen.dart
+++ lib/views/screens/navigation/dashboard_screen.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:labor/controllers/auth_controller.dart';
+import 'package:labor/controllers/dashboard_controller.dart';
 import 'package:labor/models/task_model.dart';
 import 'package:labor/services/firebase_services.dart';
 import 'package:labor/utils/app_helper.dart';
+import 'package:labor/utils/extentions.dart';
 import 'package:labor/views/widgets/bottom_sheets/work_spaces_list.dart';
 import 'package:labor/views/widgets/cards/task_card.dart';
 import 'package:labor/views/widgets/cards/workspace_card.dart';
 import 'package:paginate_firestore_plus/paginate_firestore.dart';
 
 class DashboardScreen extends StatelessWidget {
-  const DashboardScreen({super.key});
+  DashboardController dashboardController = Get.put(DashboardController());
+  DashboardScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +24,7 @@ class DashboardScreen extends StatelessWidget {
           child: GetBuilder<AuthController>(
             builder: (controller) {
               return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   controller.selectedWorkSpace == null
                       ? SizedBox()
@@ -36,6 +40,19 @@ class DashboardScreen extends StatelessWidget {
                           workspaceModel: controller.selectedWorkSpace!,
                         ),
                       ),
+                  20.height,
+                  Text(
+                    'Stats',
+                    style: context.textTheme.titleLarge?.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'Recent Tasks',
+                    style: context.textTheme.titleLarge?.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                   PaginateFirestore(
                     itemBuilder: (ctx, docs, index) {
                       final TaskModel taskModel = TaskModel.fromDoc(
