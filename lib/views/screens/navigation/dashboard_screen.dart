ec64d4637c4175cf6b7fef4e69dcf6a342d923ee
Gul Taskeen <gultaskeen600@gmail.com>
2025-04-19 20:29:03 +0500

    tasks duration

--- lib/views/screens/navigation/dashboard_screen.dart
+++ lib/views/screens/navigation/dashboard_screen.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:labor/controllers/auth_controller.dart';
+import 'package:labor/models/task_model.dart';
+import 'package:labor/services/firebase_services.dart';
 import 'package:labor/utils/app_helper.dart';
 import 'package:labor/views/widgets/bottom_sheets/work_spaces_list.dart';
+import 'package:labor/views/widgets/cards/task_card.dart';
 import 'package:labor/views/widgets/cards/workspace_card.dart';
+import 'package:paginate_firestore_plus/paginate_firestore.dart';
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
@@ -14,11 +18,11 @@ class DashboardScreen extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(12),
-          child: Column(
-            children: [
-              GetBuilder<AuthController>(
-                builder: (controller) {
-                  return controller.selectedWorkSpace == null
+          child: GetBuilder<AuthController>(
+            builder: (controller) {
+              return Column(
+                children: [
+                  controller.selectedWorkSpace == null
                       ? SizedBox()
                       : InkWell(
                         onTap: () {
@@ -31,10 +35,24 @@ class DashboardScreen extends StatelessWidget {
                         child: WorkspaceCard(
                           workspaceModel: controller.selectedWorkSpace!,
                         ),
+                      ),
+                  PaginateFirestore(
+                    itemBuilder: (ctx, docs, index) {
+                      final TaskModel taskModel = TaskModel.fromDoc(
+                        docs[index],
                       );
-                },
-              ),
-            ],
+                      return TaskCard(taskModel: taskModel);
+                    },
+                    shrinkWrap: true,
+                    query: FirebaseServices().myTasksQuery(
+                      controller.selectedWorkSpace?.ref?.id ?? '',
+                    ),
+                    isLive: true,
+                    itemBuilderType: PaginateBuilderType.listView,
+                  ),
+                ],
+              );
+            },
           ),
         ),
       ),
