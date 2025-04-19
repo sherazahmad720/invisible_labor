6a86d7d3fb686b2abff488a24a54aaff648b0ad7
Sherazi <sherazahmad720@gmail.com>
2025-04-19 13:54:41 +0500

    basic controller setup done

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,3 +1,86 @@
-import 'package:get/get.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class AuthController extends GetxController {}
+import 'package:get/get.dart';
+import 'package:labor/models/user_model.dart';
+import 'package:labor/views/screens/auth/login_screen.dart';
+import 'package:labor/views/screens/navigation_screen.dart';
+
+class AuthController extends GetxController {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final Rx<User?> user = Rx<User?>(null);
+  UserModel? userModel;
+
+  @override
+  Future<void> onInit() async {
+    super.onInit();
+    user.value = _auth.currentUser;
+
+    await Future.delayed(const Duration(milliseconds: 1500));
+
+    _auth.authStateChanges().listen((User? firebaseUser) async {
+      if (firebaseUser != null) {
+        user.value = firebaseUser;
+        await fetchUserModel();
+
+        Get.offAll(() => NavigationScreen());
+      } else {
+        Get.offAll(() => LoginScreen());
+      }
+    });
+  }
+
+  Future<String?> login(String email, String password) async {
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+      // await FirebaseMessaging.instance.subscribeToTopic(
+      //   FirebaseAuth.instance.currentUser!.uid,
+      // );
+      return null;
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    } catch (e) {
+      return 'An unexpected error occurred. Please try again.';
+    }
+  }
+
+  Future<void> logout() async {
+    // await FirebaseMessaging.instance.unsubscribeFromTopic(
+    //   FirebaseAuth.instance.currentUser!.uid,
+    // );
+    await _auth.signOut();
+  }
+
+  bool isLoggedIn() {
+    return _auth.currentUser != null;
+  }
+
+  String? getUserId() {
+    return _auth.currentUser?.uid;
+  }
+
+  String? getUserEmail() {
+    return _auth.currentUser?.email;
+  }
+
+  Future<UserModel?> fetchUserModel() async {
+    String? userId = getUserId();
+    if (userId != null) {
+      DocumentSnapshot doc =
+          await FirebaseFirestore.instance
+              .collection('users')
+              .doc(userId)
+              .get();
+      if (doc.exists) {
+        userModel = UserModel.fromDoc(doc);
+        update();
+        return userModel!;
+      } else {
+        logout();
+      }
+    } else {
+      logout();
+    }
+    return null;
+  }
+}
--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,1 +1,51 @@
-class UserModel {}
+// User Information
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class UserModel {
+  final String id;
+  final String name;
+  final String phone;
+  final String? photoUrl;
+  final String email;
+  final bool? isBlocked;
+
+  UserModel({
+    required this.id,
+    required this.name,
+    required this.phone,
+    this.photoUrl,
+    required this.email,
+    this.isBlocked,
+  });
+  factory UserModel.fromDoc(DocumentSnapshot doc) {
+    var data = doc.data() as Map<String, dynamic>;
+    return UserModel(
+      id: doc.id, // Corrected from json.id to data['id']
+      name: data['name'],
+      phone: data['phone'],
+      photoUrl: data['photoUrl'],
+      email: data['email'],
+      isBlocked: data['isBlocked'] ?? false,
+    );
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> doc) {
+    return UserModel(
+      id: doc['id'],
+      name: doc['name'],
+      phone: doc['phone'],
+      photoUrl: doc['photoUrl'],
+      email: doc['email'],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'phone': phone,
+      'photoUrl': photoUrl,
+      'email': email,
+    };
+  }
+}
